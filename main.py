b49887aaab16579f0a67e584ae8bc1eb8d6b54ec
Simone <simonequalcosadesantis@gmail.com>
2022-01-24 12:08:24 +0100

    Test version NO PRE NO FLAG

--- main.py
+++ main.py
@@ -1,30 +1,4 @@
 from QMF import *
-import sys
-
-opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
-args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
-
-if len(args) != 3:
-    raise SystemExit(f'Usage: {sys.argv[0]} [-g] COLORSIZE LAMBDA EPSILON')
-
-if "-g" in opts:
-    generate_flag = True
-else:
-    generate_flag = False
-
-
-
-# PARAMETERS
-color_size = int(args[0])
-coordinate_size = 2
-lambda_par = int(args[1])
-epsilon = int(args[2])
-optimization = 0
-print("###")
-print(f"COLORSIZE: {color_size}")
-print(f"LAMBDA: {lambda_par}")
-print(f"EPSILON: {epsilon}")
-print("###")
 
 # INPUT DIRECTORY
 input_dir = "./images/"
@@ -41,7 +15,6 @@ images["TEST_8x8"] = "gray_8.png"
 images["GRAY_8x8"] = "gray_shade_8.png"
 images["CHAPLIN"] = "chaplin_64.png"
 filename = images["TEST_8x8"]  # Change This One
-
 # CONVERSION
 print("Converting image into array")
 img = Converter.to_array(f'{input_dir}{filename}')
@@ -53,49 +26,45 @@ converged_patches = dict()
 for pos in patches.keys():
     converged_patches[pos] = False
 
-# SIMULATOR
-print("Setting simulator up")
-sim = Simulator(mps_max_bond_dimension=None)
-
-# PRE-TRANSPILING
-qmf = QuantumMedianFilter()
-if generate_flag:
-    qmf.generate(sim, color_size, coordinate_size, optimization)
+# PARAMETERS
+color_size = 4
+lambda_par = 16
+epsilon = 16
+iter = 0
 
 # EXECUTION
-iteration = 0
 start = time.time()
 while list(converged_patches.values()).count(False) != 0:
 
-    iteration += 1
+    iter += 1
 
     for pos, patch in patches.items():
 
-        print(f'ITER: {iteration}')
+        print(f'ITER: {iter}')
         print(f'PATCH: {pos}')
 
         # CIRCUIT
         print("Building the circuit")
-        neqr = Circuit.neqr(patch, color_num=color_size, verbose=False)
-        neqr_transpiled = sim.transpile(neqr, optimization=0, verbose=True)
-        qmf.prepare(np.array(patch), lambda_par, color_size, neqr_transpiled)
+        qmf = QuantumMedianFilter()
+        qmf.prepare(np.array(patch), lambda_par, color_size)
         circuit = qmf.get()
 
         # RUN
-        # qobj = sim.transpile(circuit, optimization=0, verbose=True)
+        print("Setting simulator up")
+        sim = Simulator(mps_max_bond_dimension=None)
+        qobj = sim.transpile(circuit, optimization=0, verbose=True)
         # qobj = load_qasm(f'{qasm_dir}{circuit.name}')
-        qobj = circuit
         answer = sim.simulate(qobj, shots=128, verbose=True)
 
         # OUTPUT
         out = patch.copy()
         out = Converter.decode_image(answer, out, color_size=color_size)
-        # Converter.to_image(out, filename=f'{output_dir}patch_{pos[0]}{pos[1]}_{iteration}.png')
+        # Converter.to_image(out, filename=f'{output_dir}patch_{pos[0]}{pos[1]}_{iter}.png')
         res[pos] = out
 
     converged_patches = patcher.converged_patches(patches, res, epsilon)
     new = patcher.convert_patches(res)
-    output = f'{output_dir}output_{iteration}.png'
+    output = f'{output_dir}output_{iter}.png'
     Converter.to_image(new, filename=output)
     patcher.load_image(new)
     patches = patcher.get_patches()
