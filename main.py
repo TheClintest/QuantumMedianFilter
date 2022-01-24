8e8c5c3d2d173c9872e141c1ada6f57fc0760084
Simone <simonequalcosadesantis@gmail.com>
2022-01-24 19:24:33 +0100

    Base version

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ color_size = int(args[0])
 coordinate_size = 2
 lambda_par = int(args[1])
 epsilon = int(args[2])
-optimization = 1
+optimization = 0
 
 print("###")
 print(f"COLORSIZE: {color_size}")
